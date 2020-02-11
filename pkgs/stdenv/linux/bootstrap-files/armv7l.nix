1e98b965f5c7f5f20ee76767caa8754640abd5e0
Andrew Childs <lorne@cons.org.nz>
2020-02-11 18:08:12 +0900

    TESTING use tools that will be built by hydra after nixpkgs pr

--- pkgs/stdenv/linux/bootstrap-files/armv7l.nix
+++ pkgs/stdenv/linux/bootstrap-files/armv7l.nix
@@ -1,14 +1,17 @@
 {
-  # https://hydra.nixos.org/build/111556965
+  # built from https://github.com/NixOS/nixpkgs/pull/79793
+  # cherry-picked onto master for cross fixes (infinite recursion)
+  # uploaded to s3
+  # only for testing locally
+
   busybox = import <nix/fetchurl.nix> {
-    url = https://s3.ap-northeast-1.amazonaws.com/nix-misc.cons.org.nz/bryxbn910b5cf1q54yxsw4dzyckdh3dy-busybox;
+    url = https://s3.ap-northeast-1.amazonaws.com/nix-misc.cons.org.nz/h3l1fq2zi9xw56wjmdkr8j0m9g8f3w58-stdenv-bootstrap-tools-armv7l-unknown-linux-gnueabihf/on-server/busybox;
     sha256 = "18qc6w2yykh7nvhjklsqb2zb3fjh4p9r22nvmgj32jr1mjflcsjn";
     executable = true;
   };
 
-  # https://hydra.nixos.org/build/111556911
   bootstrapTools = import <nix/fetchurl.nix> {
-    url = https://s3.ap-northeast-1.amazonaws.com/nix-misc.cons.org.nz/dnd817yvqvbz3p77km228px9r9a7wc9n-bootstrap-tools.tar.xz;
-    sha256 = "0l0544ixi9x2y23c9fmwyvb1y9kd9gb3yamnr73mdf5f49vjvykr";
+    url = https://s3.ap-northeast-1.amazonaws.com/nix-misc.cons.org.nz/h3l1fq2zi9xw56wjmdkr8j0m9g8f3w58-stdenv-bootstrap-tools-armv7l-unknown-linux-gnueabihf/on-server/bootstrap-tools.tar.xz;
+    sha256 = "1vgmx4075379cpj9ivskl1aarbdcm59qzvgh7vgjqxh9rww2j2iw";
   };
 }
