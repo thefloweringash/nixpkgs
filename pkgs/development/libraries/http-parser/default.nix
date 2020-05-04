1031455b46f512125072c0432adcca8b70c9b77e
Andrew Childs <lorne@cons.org.nz>
2020-05-04 16:23:09 +0900

    http-parser: fix tests on 32-bit platforms

--- pkgs/development/libraries/http-parser/default.nix
+++ pkgs/development/libraries/http-parser/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchFromGitHub }:
+{ stdenv, fetchFromGitHub, fetchpatch }:
 
 let
   version = "2.9.4";
@@ -14,7 +14,16 @@ in stdenv.mkDerivation {
   };
 
   NIX_CFLAGS_COMPILE = "-Wno-error";
-  patches = [ ./build-shared.patch ];
+  patches = [
+    ./build-shared.patch
+
+    # Fixes tests on 32-bit platforms
+    # https://github.com/nodejs/http-parser/pull/510
+    (fetchpatch {
+      url = "https://github.com/nodejs/http-parser/commit/0e5868aebb9eb92b078d27bb2774c2154dc167e2.patch";
+      sha256 = "0jpg1v1wy7pz9sx0pmvcs1498bhk0dsprbw5fnis6k9nshlg54n5";
+    })
+  ];
   makeFlags = [ "DESTDIR=" "PREFIX=$(out)" ];
   buildFlags = [ "library" ];
   doCheck = true;
