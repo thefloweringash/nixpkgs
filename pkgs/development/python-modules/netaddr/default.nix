23f9efd8bbf73b36430bcd5a2753e2de45776b8c
Andrew Childs <lorne@cons.org.nz>
2020-09-12 14:24:03 +0900

    pythonPackages.netaddr: 0.7.19 -> 0.7.20
    Fixes tests on macOS Catalina and newer:
    
            #   inet_pton has to be different on Mac OSX *sigh*
            assert IPAddress('010.000.000.001', flags=INET_PTON) == IPAddress('10.0.0.1')
    >       assert int_to_str(0xffff) == '::0.0.255.255'
    E       AssertionError: assert '::ffff' == '::0.0.255.255'
    E         - ::0.0.255.255
    E         + ::ffff

--- pkgs/development/python-modules/netaddr/default.nix
+++ pkgs/development/python-modules/netaddr/default.nix
@@ -8,11 +8,11 @@
 
 buildPythonPackage rec {
   pname = "netaddr";
-  version = "0.7.19";
+  version = "0.7.20";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "38aeec7cdd035081d3a4c306394b19d677623bf76fa0913f6695127c7753aefd";
+    sha256 = "0gkgfxjakfkagybjzrak29937sx6izmfif3yswaki4f3mvjm54nh";
   };
 
   LC_ALL = "en_US.UTF-8";
@@ -24,13 +24,6 @@ buildPythonPackage rec {
       -k 'not test_ip_splitter_remove_prefix_larger_than_input_range' \
       netaddr/tests
   '';
-
-  patches = [
-    (fetchpatch {
-      url = "https://github.com/drkjam/netaddr/commit/2ab73f10be7069c9412e853d2d0caf29bd624012.patch";
-      sha256 = "0s1cdn9v5alpviabhcjmzc0m2pnpq9dh2fnnk2x96dnry1pshg39";
-    })
-  ];
 
   meta = with stdenv.lib; {
     homepage = "https://github.com/drkjam/netaddr/";
