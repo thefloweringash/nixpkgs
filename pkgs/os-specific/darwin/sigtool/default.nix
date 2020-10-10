1ae886a1c025e0c24094f3b13813bec15676b012
Andrew Childs <lorne@cons.org.nz>
2020-10-10 17:26:22 +0900

    fixup! [wip] bootstrap tools is so close

--- pkgs/os-specific/darwin/sigtool/default.nix
+++ pkgs/os-specific/darwin/sigtool/default.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation {
     owner = "thefloweringash";
     repo = "sigtool";
     rev = "3c447d5ba14f912b76ac2c876d29a201377142fb";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+    sha256 = "017abmvl6mf83ij7ak4q5frv235fcahdlbn74fjh2rkk0aknzigk";
   };
 
   nativeBuildInputs = [ pkg-config cmake ];
