8dc5668500aabed878c67ee95ae036125b7ea20a
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-10-29 01:55:07 +0100

    python3Packages.pyobjc-framework-CalendarStore: init at 6.2.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5293,10 +5293,6 @@ in {
 
   pyobjc-framework-Accounts = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-Accounts { });
 
-  pyobjc-framework-AVFoundation = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-AVFoundation { });
-
-  pyobjc-framework-AVKit = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-AVKit { });
-
   pyobjc-framework-AddressBook = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-AddressBook { });
 
   pyobjc-framework-AVFoundation = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-AVFoundation { });
