e6118fd880fb97555066afa08e2b5bcedfc2c41c
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-10-29 01:25:44 +0100

    python3Packages.pyobjc-framework-Security: init at 6.2.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5353,6 +5353,8 @@ in {
 
   pyobjc-framework-SearchKit = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-SearchKit { });
 
+  pyobjc-framework-Security = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-Security { });
+
   pyobjc-framework-ServiceManagement = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-ServiceManagement { });
 
   pyobjc-framework-Speech = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-Speech { });
