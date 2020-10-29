fdb6d8fb515ddd7adde31812d3880be9724d8048
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-10-29 02:58:51 +0100

    python3Packages.pyobjc-framework-ExecutionPolicy: init at 6.2.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5337,6 +5337,8 @@ in {
 
   pyobjc-framework-ExceptionHandling = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-ExceptionHandling { });
 
+  pyobjc-framework-ExecutionPolicy = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-ExecutionPolicy { });
+
   pyobjc-framework-FinderSync = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-FinderSync { });
 
   pyobjc-framework-FSEvents = enabledIfDarwin (callPackage ../development/python-modules/pyobjc-framework-FSEvents { });
