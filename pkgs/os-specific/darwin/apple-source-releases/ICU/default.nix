311a3558baac2db39f2ced620789ee3361d67301
Andrew Childs <lorne@cons.org.nz>
2020-11-19 17:21:44 +0900

    darwin/ICU: cross compilation

--- pkgs/os-specific/darwin/apple-source-releases/ICU/default.nix
+++ pkgs/os-specific/darwin/apple-source-releases/ICU/default.nix
@@ -1,7 +1,9 @@
-{ appleDerivation }:
+{ appleDerivation, stdenv, buildPackages }:
 
 appleDerivation {
   patches = [ ./clang-5.patch ];
+
+  depsBuildBuild = [ buildPackages.stdenv.cc ];
 
   postPatch = ''
     substituteInPlace makefile \
@@ -12,9 +14,26 @@ appleDerivation {
       --replace '-install_name $(libdir)' "-install_name $out/lib/" \
       --replace /usr/local/bin/ /bin/ \
       --replace /usr/lib/ /lib/ \
+  '' + stdenv.lib.optionalString (stdenv.buildPlatform != stdenv.hostPlatform) ''
+
+    # This looks like a bug in the makefile. It defines ENV_BUILDHOST to
+    # propagate the correct value of CC, CXX, etc, but has the following double
+    # expansion that results in the empty string.
+    substituteInPlace makefile \
+      --replace '$($(ENV_BUILDHOST))' '$(ENV_BUILDHOST)'
   '';
 
-  makeFlags = [ "DSTROOT=$(out)" ];
+  makeFlags = [ "DSTROOT=$(out)" ]
+    ++ stdenv.lib.optionals (stdenv.buildPlatform != stdenv.hostPlatform) [
+      "CROSS_BUILD=YES"
+      "BUILD_TYPE="
+      "RC_ARCHS=${stdenv.hostPlatform.darwinArch}"
+      "HOSTCC=cc"
+      "HOSTCXX=c++"
+      "CC=${stdenv.cc.targetPrefix}cc"
+      "CXX=${stdenv.cc.targetPrefix}c++"
+      "HOSTISYSROOT="
+    ];
 
   postInstall = ''
     mv $out/usr/local/include $out/include
