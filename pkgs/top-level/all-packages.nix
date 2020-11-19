d741ec23a9c897b83273e2d92c471f7dacd89505
Andrew Childs <lorne@cons.org.nz>
2020-11-19 17:25:00 +0900

    bash: provide a working binutils
    Why does bash need binutils at all? What's wrong with stdenv?
    
    This avoids a broken ld wrapper, since binutils is binutils wrapped
    for linux, even though it's only required on darwin.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8531,7 +8531,9 @@ in
 
   any-nix-shell = callPackage ../shells/any-nix-shell { };
 
-  bash = lowPrio (callPackage ../shells/bash/4.4.nix { });
+  bash = lowPrio (callPackage ../shells/bash/4.4.nix {
+    binutils = stdenv.cc.bintools;
+  });
   bash_5 = lowPrio (callPackage ../shells/bash/5.0.nix { });
   bashInteractive_5 = lowPrio (callPackage ../shells/bash/5.0.nix {
     interactive = true;
@@ -8540,6 +8542,7 @@ in
 
   # WARNING: this attribute is used by nix-shell so it shouldn't be removed/renamed
   bashInteractive = callPackage ../shells/bash/4.4.nix {
+    binutils = stdenv.cc.bintools;
     interactive = true;
     withDocs = true;
   };
