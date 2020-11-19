28408eeca0b7268b3f2a1a6fa45eb55210be6e73
Andrew Childs <lorne@cons.org.nz>
2020-11-19 17:00:24 +0900

    llvm_10: cross compilation and apple silicon support

--- pkgs/development/compilers/llvm/10/libc++abi/default.nix
+++ pkgs/development/compilers/llvm/10/libc++abi/default.nix
@@ -1,6 +1,11 @@
 { lib, stdenv, cmake, fetch, libcxx, libunwind, llvm, version
 , enableShared ? !stdenv.hostPlatform.isStatic
+, standalone ? false
 }:
+
+let
+  darwinCross = stdenv.hostPlatform.isDarwin && (stdenv.hostPlatform != stdenv.buildPlatform);
+in
 
 stdenv.mkDerivation {
   pname = "libc++abi";
@@ -11,8 +16,9 @@ stdenv.mkDerivation {
   nativeBuildInputs = [ cmake ];
   buildInputs = lib.optional (!stdenv.isDarwin && !stdenv.isFreeBSD && !stdenv.hostPlatform.isWasm) libunwind;
 
-  cmakeFlags = lib.optionals (stdenv.hostPlatform.useLLVM or false) [
+  cmakeFlags = lib.optionals ((stdenv.hostPlatform.useLLVM or false) || darwinCross || standalone) [
     "-DLLVM_ENABLE_LIBCXX=ON"
+  ] ++ lib.optionals (stdenv.hostPlatform.useLLVM or false) [
     "-DLIBCXXABI_USE_LLVM_UNWINDER=ON"
   ] ++ lib.optionals stdenv.hostPlatform.isWasm [
     "-DLIBCXXABI_ENABLE_THREADS=OFF"
@@ -28,7 +34,7 @@ stdenv.mkDerivation {
     unpackFile ${llvm.src}
     cmakeFlags+=" -DLLVM_PATH=$PWD/$(ls -d llvm-*) -DLIBCXXABI_LIBCXX_PATH=$PWD/$(ls -d libcxx-*)"
   '' + lib.optionalString stdenv.isDarwin ''
-    export TRIPLE=x86_64-apple-darwin
+    export TRIPLE=${stdenv.hostPlatform.config}
   '' + lib.optionalString stdenv.hostPlatform.isMusl ''
     patch -p1 -d $(ls -d libcxx-*) -i ${../libcxx-0001-musl-hacks.patch}
   '' + lib.optionalString stdenv.hostPlatform.isWasm ''
@@ -42,7 +48,7 @@ stdenv.mkDerivation {
         # the magic combination of necessary CMake variables
         # if you fancy a try, take a look at
         # https://gitlab.kitware.com/cmake/community/-/wikis/doc/cmake/RPATH-handling
-        install_name_tool -id $out/$file $file
+        ${stdenv.cc.targetPrefix}install_name_tool -id $out/$file $file
       done
       make install
       install -d 755 $out/include
