64b6c42678410615e8e48cde12c9457a8dfd1b3d
Andrew Childs <lorne@cons.org.nz>
2021-02-12 12:11:43 +0900

    mc: fix code signature on aarch64-darwin

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6304,7 +6304,9 @@ in
 
   mbutil = python3Packages.callPackage ../applications/misc/mbutil { };
 
-  mc = callPackage ../tools/misc/mc { };
+  mc = callPackage ../tools/misc/mc {
+    inherit (darwin) autoSignDarwinBinariesHook;
+  };
 
   mcabber = callPackage ../applications/networking/instant-messengers/mcabber { };
 
