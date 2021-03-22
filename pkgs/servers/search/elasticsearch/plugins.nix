549af33c802eb8b72a17298c423058c119173def
Andrew Childs <lorne@cons.org.nz>
2021-03-22 14:44:43 +0900

    elasticsearch/plugins: build with "modules" from elasticsearch
    Fixes dependency error while building search-guard:
    
        Exception in thread "main" java.lang.IllegalArgumentException: Missing plugin [lang-painless], dependency of [search-guard-7]

--- pkgs/servers/search/elasticsearch/plugins.nix
+++ pkgs/servers/search/elasticsearch/plugins.nix
@@ -8,9 +8,9 @@ let
     installPhase ? ''
       mkdir -p $out/config
       mkdir -p $out/plugins
-      ln -s ${elasticsearch}/lib $out/lib
+      ln -s ${elasticsearch}/lib ${elasticsearch}/modules $out
       ES_HOME=$out ${elasticsearch}/bin/elasticsearch-plugin install --batch -v file://$src
-      rm $out/lib
+      rm $out/lib $out/modules
     '',
     ...
   }:
